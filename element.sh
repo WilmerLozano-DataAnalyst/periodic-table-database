571d38f509c55e5de9daeb15bb0ae7e5dd63ed8d
Wilmer Lozano <wilmer0298@gmail.com>
2024-11-18 16:57:20 +0000

    fix: create element.sh script with querying functionality

--- element.sh
+++ element.sh
@@ -2,13 +2,22 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+# Verificar si hay un argumento
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit
 fi
 
-QUERY_RESULT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number INNER JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number=$1 OR elements.symbol='$1' OR elements.name='$1'")
+# Consultar la base de datos según el argumento
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  # Si es un número (atomic_number)
+  QUERY_RESULT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number INNER JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number=$1")
+else
+  # Si es un símbolo o un nombre (requiere comillas simples)
+  QUERY_RESULT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number INNER JOIN types ON properties.type_id=types.type_id WHERE elements.symbol='$1' OR elements.name='$1'")
+fi
 
+# Verificar si hay resultados
 if [[ -z $QUERY_RESULT ]]; then
   echo "I could not find that element in the database."
 else
