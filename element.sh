dbcf1c520d840574d35766ac8dc399db544aada9
Wilmer Lozano <wilmer0298@gmail.com>
2024-11-18 16:49:30 +0000

    feat: create element.sh script with querying functionality

--- element.sh
+++ element.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+QUERY_RESULT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number INNER JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number=$1 OR elements.symbol='$1' OR elements.name='$1'")
+
+if [[ -z $QUERY_RESULT ]]; then
+  echo "I could not find that element in the database."
+else
+  echo "$QUERY_RESULT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING; do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+  done
+fi
